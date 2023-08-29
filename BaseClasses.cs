4f7b3297ca137a3fbb5ee880ec098fde784a5587
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-08-29 22:46:23 +0300

    Introduced Base classes

---
+++ BaseClasses.cs
@@ -0,0 +1,9 @@
+public class Point3D : Point
+{
+    public int Z { get; set; }
+
+    public Point3D(int x, int y, int z) : base(x, y)
+    {
+        Z = z;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,10 +2,16 @@
 {
     public static void Main()
     {
-        var pair = new Pair<int, string>(1, "two");
-        int i = pair.First;     //TFirst int
-        string s = pair.Second; //TSecond string
+        Point a = new(10, 20);
+        Point b = new Point3D(10, 20, 30);
 
-        Console.WriteLine($"(First int {i} Second string {s})");
+        Console.WriteLine($"(Point a.X {a.X} Point a.Y {a.Y})");
+
+        /* This somehow does not work.
+         * Looks like the document from microsoft, Point3D can refers to
+         * Point. So, seemed to be able to call b.Z. But, Not working
+         * https://learn.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/types
+         */
+        // Console.WriteLine($"(Point3D b.X {b.X} Point3D b.Y {b.Y} Point3D b.Z {b.Z})");
     }
 }
