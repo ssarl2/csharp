5be767a3eb51260b86420eea0be3856b79fb71fe
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-08-30 23:17:29 +0300

    Introduced Enums

--- Program.cs
+++ Program.cs
@@ -2,8 +2,15 @@
 {
     public static void Main()
     {
-        EditBox editBox = new();
-        Icontrol control = editBox;
-        IDataBound dataBound = editBox;
+        var turnip = SomeRootVegetable.Turnip;
+
+        var spring = Seasons.Spring;
+        var startingOnEquinox = Seasons.Spring | Seasons.Autumn;
+        var theYear = Seasons.All;
+
+        Console.WriteLine($"(turnip : {turnip})");
+        Console.WriteLine($"(spring : {spring})");
+        Console.WriteLine($"(startingOnEquinox : {startingOnEquinox})");
+        Console.WriteLine($"(theYear : {theYear})");
     }
 }
