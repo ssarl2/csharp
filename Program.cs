8fe497cbcbf18111112f8c1291d9281221b7b545
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-08-31 21:30:45 +0300

    Introduced Tuples

--- Program.cs
+++ Program.cs
@@ -1,36 +1,8 @@
 ﻿class Example
 {
-    private static bool IsNull<T>(T? v)
-    {
-        if (v == null)
-            return true;
-        return false;
-    }
-
     public static void Main()
     {
-        int? optionalInt = default;
-        string? optionalText = default;
-
-        if (IsNull(optionalInt))
-        {
-            Console.WriteLine($"(optionalInt is null)");
-        }
-        if (IsNull(optionalText))
-        {
-            Console.WriteLine($"(optionalText is null)");
-        }
-
-        optionalInt = 5;
-        optionalText = "Hello World.";
-
-        if (!IsNull(optionalText))
-        {
-            Console.WriteLine($"(optionalInt : {optionalInt})");
-        }
-        if (!IsNull(optionalText))
-        {
-            Console.WriteLine($"(optionalText : {optionalText})");
-        }
+        (double Sum, int Count) t2 = (4.5, 3);
+        Console.WriteLine($"Sum of {t2.Count} elements is {t2.Sum}.");
     }
 }
