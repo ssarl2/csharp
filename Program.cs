48117f9cc7e7c397a1797638257bf45c638125fe
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-09-06 22:49:52 +0300

    Introduced Constructors

--- Program.cs
+++ Program.cs
@@ -2,6 +2,16 @@
 {
     public static void Main()
     {
-        OverloadingExample.UsageExample();
+        /*
+         * The constructor has static modifier in the class
+         * So, it's called a static constructor
+         */
+        MyList<string> list1 = new();
+
+        /*
+         * The constructor is declared normally without any modifiers
+         * So, it's called an instance constructor
+         */
+        MyList<string> list2 = new(10);
     }
 }
