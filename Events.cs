226de2f936d29b8c3776868f0559cdd06ec30adf
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-09-08 22:37:41 +0300

    Introduce Events

---
+++ Events.cs
@@ -0,0 +1,19 @@
+class EventExample
+{
+    static int s_changeCount;
+
+    static void ListChanged(object? sender, EventArgs e)
+    {
+        s_changeCount++;
+    }
+
+    public static void Usage()
+    {
+        var names = new MyList<string>();
+        names.Changed += new EventHandler(ListChanged);
+        names.Add("Liz");
+        names.Add("Martha");
+        names.Add("Beth");
+        Console.WriteLine(s_changeCount);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,33 +2,6 @@
 {
     public static void Main()
     {
-        MyList<string> names = new();
-        names.Add("Liz");
-        names.Add("Martha");
-        names.Add("Beth");
-        for (int i = 0; i < names.Count; i++)
-        {
-            string s = names[i];
-            names[i] = s.ToUpper();
-        }
-
-        Console.WriteLine($"{names.Capacity}");
-
-        for (int i = 0; i < names.Count; i++)
-        {
-            Console.WriteLine($"{names[i]}");
-        }
-        names.Add("Abby");
-        names.Add("Boby");
-        names.Add("Cebby");
-        names.Add("Naby");
-        names.Add("Roby");
-        Console.WriteLine($"{names.Capacity}");
-
-
-        for (int i = 0; i < names.Count; i++)
-        {
-            Console.WriteLine($"{names[i]}");
-        }
+        EventExample.Usage();
     }
 }
