d4b2d036119f069295c4d9ec248754862b2c1eae
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-09-18 21:46:15 +0300

    Introduce Operators

--- a_tour_of_c_sharp/Program.cs
+++ a_tour_of_c_sharp/Program.cs
@@ -2,6 +2,14 @@
 {
     public static void Main()
     {
-        EventExample.Usage();
+        MyList<int> a = new();
+        a.Add(1);
+        a.Add(2);
+        MyList<int> b = new();
+        b.Add(1);
+        b.Add(2);
+        Console.WriteLine(a == b);  // Outputs "True"
+        b.Add(3);
+        Console.WriteLine(a == b);  // Outputs "False"
     }
 }
